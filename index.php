46329c4e440990809108fa632d41dad6c8f8f11b
unknown <dcorteswebdev@gmail.com>
2019-10-13 18:18:14 -0500

    feat: Numbers, strings and arrays

--- index.php
+++ index.php
@@ -1,12 +1,38 @@
+<?php 
+
+  $peopleOne = ['shaun', 'crystal', 'ryu'];
+
+  $peopleTwo = array('ken', 'chun-li');
+  echo $peopleTwo[1];
+
+  $ages = array(20,30,40,50);
+  $ages[1] = 25;
+  $ages[] = 60;
+
+  array_push($ages, 70);
+  // print_r($ages)
+
+  echo count($ages);
+
+  $peopeThree = array_merge($peopleOne, $peopleTwo);
+
+  // print_r($peopeThree)
+  $ninjasOne = ['shaun'=>'black', 'mario'=>'orange', 'luigi'=>'brown'];
+  echo $ninjasOne['mario'];
+
+  // print_r($ninjasOne);
+  $ninjasTwo = array('bowser'=>'green', 'peach'=>'yellow');
+  // print_r($ninjasTwo);
+  $ninjasTwo['peach'] = 'pink';
+  print_r($ninjasTwo);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>My first php file</title>
 </head>
 <body>
-  <h1><?php echo 'hello world!!!';?> </h1> 
+  <h1>User Profile page</h1> 
 </body>
 </html>
