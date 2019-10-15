945730fe7d1e2165b9fe35b4b0926cd03c6be834
unknown <dcorteswebdev@gmail.com>
2019-10-15 18:14:24 -0500

    feat: Getting data from database

--- index.php
+++ index.php
@@ -5,8 +5,25 @@ $conn = mysqli_connect('localhost', 'ninjapizza', 'test123', 'ninja_pizza');
 
 //check connection
 if(!$conn){
-  echo 'Connection error: ' . mysqli_connect_error ;
+  echo 'Connection error: ' . mysqli_connect_error() ;
 }
+
+// write query for all pizzas *
+$sql = 'SELECT title, ingredients, id FROM pizzas';
+
+// make query and get result
+$result = mysqli_query($conn, $sql);
+
+// free result from memory
+mysqli_free_result($result);
+
+// close connection
+mysqli_close($conn);
+
+// fetch the resulting rows as an array
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+print_r($pizzas);
 
 ?>
 
