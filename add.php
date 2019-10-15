4f4d762e0bbae439c068605e486ec31bf48eedbd
unknown <dcorteswebdev@gmail.com>
2019-10-15 09:44:53 -0500

    feat: Validation

--- add.php
+++ add.php
@@ -1,15 +1,38 @@
 <?php 
 
-  // if(isset($_GET['submit'])){
-  //   echo $_GET['email'];
-  //   echo $_GET['title'];
-  //   echo $_GET['ingredients'];
-  // }
-
   if(isset($_POST['submit'])){
-    echo htmlspecialchars($_POST['email']);
-    echo htmlspecialchars($_POST['title']);
-    echo htmlspecialchars($_POST['ingredients']);
+    // check email
+    if(empty($_POST['email'])){
+      echo 'An email is required <br />';
+    } else {
+      $email = $_POST['email'];
+      if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        echo 'Email must be valid email address.';
+      }
+      echo htmlspecialchars($_POST['email']);
+    }
+
+    //check title
+    if(empty($_POST['title'])){
+      echo 'A title is required <br />';
+    } else {
+      $title = $_POST['title'];
+      if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+        echo 'Title must be letters and spaces only';
+      }
+      echo htmlspecialchars($_POST['title']);
+    }
+
+    // check ingredients
+    if(empty($_POST['ingredients'])){
+      echo 'An ingredient is required <br />';
+    } else {
+      $ingredients = $_POST['ingredients'];
+      if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+        echo 'Ingredients must be a comma separated list';
+      }
+      echo htmlspecialchars($_POST['ingredients']);
+    }
   }
 
 ?>
