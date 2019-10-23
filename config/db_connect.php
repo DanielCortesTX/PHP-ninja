d7b737e08c93fa478273db58dffa4f97aa4df531
unknown <dcorteswebdev@gmail.com>
2019-10-23 08:18:16 -0500

    feat: Changed config to PDO

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,11 +1,24 @@
 <?php 
 
 // connect to database
-$conn = mysqli_connect('localhost', 'ninjapizza', 'test123', 'ninja_pizza');
+// $conn = mysqli_connect('localhost', 'ninjapizza', 'test123', 'ninja_pizza');
 
-//check connection
-if(!$conn){
-  echo 'Connection error: ' . mysqli_connect_error();
+// //check connection
+// if(!$conn){
+//   echo 'Connection error: ' . mysqli_connect_error();
+// }
+
+$servername = "localhost";
+$username = "ninjapizza";
+$password = "test123";
+
+try {
+  $conn = new PDO("mysqli:host=$servername;dbname=ninja_pizza", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo 'Connected successfully';
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
 
 ?>
